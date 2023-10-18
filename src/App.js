7e07dff90b3b74f2f7315035ababd3bb26ab7053
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-10-18 15:59:41 +0200

    Add close button

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function previous() {
     if (step > 1) setStep(step - 1);
@@ -17,32 +18,43 @@ export default function App() {
     if (step < 3) setStep(step + 1);
   }
 
+  function close() {
+    isOpen ? setIsOpen(false) : setIsOpen(true);
+  }
+
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+    <div>
+      <button className="close" onClick={close}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
 
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={previous}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={next}
-        >
-          Next
-        </button>
-      </div>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#1e789c", color: "#fff" }}
+              onClick={previous}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#1e789c", color: "#fff" }}
+              onClick={next}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
