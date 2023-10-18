93f1c9cba6ab295aeba384e2982dbf58adb44f86
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-10-18 15:14:35 +0200

    Add active class to numbers depending on step

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
+  const step = 2;
   return (
     <div className="steps">
       <div className="numbers">
         <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 1 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 1 ? "active" : ""}`}>3</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
 
       <p className="message">
